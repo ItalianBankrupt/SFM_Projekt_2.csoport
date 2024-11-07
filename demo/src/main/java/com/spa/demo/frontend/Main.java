208b250ce07f2543c695d251bebea982a610ff74
LKristof7 <125500369+LKristof7@users.noreply.github.com>
2024-11-07 15:30:38 +0100

    Spring spa start

---
+++ demo/src/main/java/com/spa/demo/frontend/Main.java
@@ -0,0 +1,16 @@
+package com.spa.demo.frontend;
+
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root= FXMLLoader.load
+    }
+}
