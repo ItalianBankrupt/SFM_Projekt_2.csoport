4b36b00f1bb59de3ceea15fde69c6e61ca1659ba
Csaba Gere <123948881+ItalianBankrupt@users.noreply.github.com>
2024-11-11 14:41:01 +0100

    Delete demo directory

--- demo/src/main/java/com/spa/demo/backend/ServicesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spa.demo.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServicesRepository extends JpaRepository<Services, Long> {
-
-}
