96fa27c238fabfca8688d9d4a10b7229b4867f21
veresattila2002 <attila20020616@gmail.com>
2024-11-14 22:15:13 +0100

    working connection between database

---
+++ demo/src/main/java/com/spa/demo/frontend/Manager.java
@@ -0,0 +1,8 @@
+package com.spa.demo.frontend;
+
+import java.util.List;
+
+public interface Manager {
+    public void startBackend();
+    public void stopBackend();
+}
---
+++ demo/src/main/java/com/spa/demo/frontend/Restaurant/Food.java
@@ -0,0 +1,25 @@
+package com.spa.demo.frontend.Restaurant;
+
+public class Food {
+    private String name;
+    private int price;
+    private Restaurant.Type type;
+
+    public Food(String name, int price, Restaurant.Type type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Restaurant.Type getType() {
+        return type;
+    }
+}
