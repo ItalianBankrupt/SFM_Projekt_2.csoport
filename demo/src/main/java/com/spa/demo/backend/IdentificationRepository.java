be3ae910d1ee6bb2ba079c34d48d60fdfc3b1162
veresattila2002 <125447298+veresattila2002@users.noreply.github.com>
2024-11-14 16:51:12 +0100

    Add files via upload

---
+++ demo/src/main/java/com/spa/demo/backend/IdentificationRepository.java
@@ -0,0 +1,9 @@
+package com.spa.demo.backend;
+
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IdentificationRepository extends JpaRepository<Identification, Long> {
+}
