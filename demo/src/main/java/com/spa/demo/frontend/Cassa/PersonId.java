24516ed5367ed50d4beef7b85a3962e990c6e019
veresattila2002 <attila20020616@gmail.com>
2024-11-17 13:34:08 +0100

    PersonId class added

---
+++ demo/src/main/java/com/spa/demo/frontend/Cassa/PersonId.java
@@ -0,0 +1,41 @@
+package com.spa.demo.frontend.Cassa;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonId
+{
+    //Infos from buyerClass
+    private String id;
+    private String buyerId;
+
+    //Tickets and Services 0-if doesn't have that type 1-if have
+    private int AdultFelling = 0;
+    private int StudentFelling = 0;
+    private int ChildTicket = 0;
+    private int BeachTicket = 0;
+    private int RetiredTicket = 0;
+    private int StudendAndChildTicket = 0;
+    private int Sauna = 0;
+    private int SafeDeposit = 0;
+    private int Lounger = 0;
+    private int SunBed = 0;
+    private int SunBedOnBeach = 0;
+    private int Baldachin = 0;
+    private int AquaParkMinimal = 0;
+    private int AquaParkNormal = 0;
+    private int PremiumTicket = 0;
+    private int Locker = 0;
+
+    //Balance info
+    private int Balance = 0;
+
+    public PersonId(String id,String buyerId)
+    {
+        this.id = id;
+        this.buyerId = buyerId;
+    }
+}
