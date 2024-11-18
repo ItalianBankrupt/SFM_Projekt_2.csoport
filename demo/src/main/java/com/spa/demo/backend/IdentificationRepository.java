8d7520d59a7135f6ee6fcabc14f19fe9c5172ba8
LKristof7 <125500369+LKristof7@users.noreply.github.com>
2024-11-18 19:20:36 +0100

    Delete demo directory
    Necessary

--- demo/src/main/java/com/spa/demo/backend/IdentificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spa.demo.backend;
-
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IdentificationRepository extends JpaRepository<Identification, Long> {
-}
