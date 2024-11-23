21b6d4d4b806e4c415294cb8b54a63671387e98a
ItalianBankrupt <csabagere14@gmail.com>
2024-11-23 10:18:06 +0100

    Added money to Identification and Query methot to IdentificationRepository

--- demo/src/main/java/com/spa/demo/backend/IdentificationRepository.java
+++ demo/src/main/java/com/spa/demo/backend/IdentificationRepository.java
@@ -1,9 +1,16 @@
 package com.spa.demo.backend;
 
 import jakarta.persistence.Id;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IdentificationRepository extends JpaRepository<Identification, String> {
+    @Transactional
+    @Modifying
+    @Query("update Identification  i set i.money = :money where i.PersonId = :personID")
+    int updateMoneyForPerson(int money, String personId);
 }
